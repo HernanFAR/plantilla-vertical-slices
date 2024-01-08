d01dfddbe595afdc339569933ecfc8cc017b13d0
Hernan <h.f.alvarez.rubio>
2024-01-08 11:50:29 -0300

    feat: added logging in the exceptionHandlingBehavior

--- src/shared/Shared.CrossCutting/ExceptionHandling/ExceptionHandlingBehavior.cs
+++ src/shared/Shared.CrossCutting/ExceptionHandling/ExceptionHandlingBehavior.cs
@@ -1,4 +1,5 @@
-﻿using Shared.Abstracts.Requests;
+﻿using Microsoft.Extensions.Logging;
+using Shared.Abstracts.Requests;
 using Shared.Abstracts.Responses;
 using Shared.CrossCutting.Abstracts;
 
@@ -9,9 +10,13 @@ namespace Shared.CrossCutting.ExceptionHandling;
 /// </summary>
 /// <typeparam name="TRequest">The intercepted request to handle</typeparam>
 /// <typeparam name="TResult">The expected successful response</typeparam>
-public sealed class ExceptionHandlingBehavior<TRequest, TResult> : IPipelineBehavior<TRequest, TResult>
+public sealed class ExceptionHandlingBehavior<TRequest, TResult>(
+    ILogger<TResult> logger) 
+    : IPipelineBehavior<TRequest, TResult>
     where TRequest : IBaseRequest<TResult>
 {
+    private readonly ILogger<TResult> _logger = logger;
+
     /// <inheritdoc/>
     public async ValueTask<Result<TResult>> HandleAsync(TRequest request, RequestHandlerDelegate<TResult> next, 
         CancellationToken cancellationToken = default)
@@ -39,6 +44,8 @@ public sealed class ExceptionHandlingBehavior<TRequest, TResult> : IPipelineBeha
     /// <returns>A <see cref="ValueTask"/> representing the processing of the exception</returns>
     internal ValueTask ProcessExceptionAsync(Exception ex, TRequest request)
     {
+        _logger.LogCritical(ex, "Ha ocurrido una excepción no manejada en el sistema");
+
         return ValueTask.CompletedTask;
     }
     
